86173e1a4f9254e33e06640cad39ca77a129a8aa
Sasha Johnson <johnson.sashamarie@gmail.com>
2020-08-01 12:44:03 +0800

    Modified Files

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -7,14 +7,39 @@ from .models import NewsStory
 class StoryForm(ModelForm):
     class Meta:
         model = NewsStory
-        fields = ['title', 'author', 'pub_date', 'content']
+        fields = ['title', 'author', 'pub_date', 'content', 'image']
         widgets = {
             'pub_date': forms.DateInput(
                 format=('%m/%d/%Y'),
                 attrs={
-                    'class': 'form-control',
+                    'class': 'form',
                     'placeholder': 'Select a date',
+                    'class': 'inputbox',
                     'type': 'date'
+                }
+            ), 
+
+            'title': forms.TextInput(
+                attrs={
+                    'class': 'form',
+                    'class': 'inputbox',
+                    'placeholder': 'Title Name'
+                }
+            ),
+            
+            'author': forms.TextInput(
+                attrs={
+                    'class': 'form',
+                    'class': 'inputbox',
+                    'placeholder': 'Author Name'
+                }
+            ),
+            
+            'content': forms.Textarea(
+                attrs={
+                    'class': 'form', 
+                    'class': 'inputbox',
+                    'placeholder': 'Enter Content Here'
                 }
             )
         }
